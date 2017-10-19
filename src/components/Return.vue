d7dddcb1b43ce8ad4d5aad104af3a98fe34c924d
johnhunsley <snowball33>
2017-10-19 17:25:09 +0100

    returns page

--- src/components/Return.vue
+++ src/components/Return.vue
@@ -1,7 +1,23 @@
 <template>
 <div>
 <h4 v-if="authenticated && admin">
-  Submit a catch return
+  <p>Submit a catch return</p>
+  <div id="selectFishery">
+    <p>
+      <select v-model="fishery">
+        <option disabled selected>select fishery</option>
+        <option>Wroxeter</option>
+        <option>Atcham (Below Tern)</option>
+        <option>Atcham (Above Tern)</option>
+        <option>Rossall Grange</option>
+        <option>Montford Bridge</option>
+      </select>
+    </p>
+  </div>
+  <div id="catchDetails" v-if="fishery">
+    <p>Enter details for catches at {{fishery}}</p>
+    <p>Date: <datepicker></datepicker></p>
+  </div>
 </h4>
 <h4 v-if="!authenticated">
   You are not logged in! Please <a @click="auth.login()">Log In</a> to continue.
@@ -10,9 +26,24 @@
 </template>
 
 <script>
+import datepicker from 'vuejs-datepicker'
+
 export default {
-  name: 'admin',
-  props: ['auth', 'authenticated', 'admin']
+  name: 'return',
+  components: {
+    datepicker
+  },
+  props: ['auth', 'authenticated', 'admin'],
+  data () {
+    return {
+      fishery: '',
+      state: {
+        disabled: {
+          from: new Date()
+        }
+      }
+    }
+  }
 }
 </script>
 
