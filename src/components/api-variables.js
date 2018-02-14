c9139e285e6520946118d7e825b2b3e2f4be15d7
johnhunsley <snowball33>
2018-02-14 12:44:39 +0000

    new api heroku domain

--- src/components/api-variables.js
+++ src/components/api-variables.js
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
   // baseUrl: process.env.API_BASE_URL
   // baseUrl: 'http://localhost:8080'
-  // baseUrl: 'http://lymm.stateless-services.com:8080'
-  baseUrl: 'https://lymm-returns-api.herokuapp.com'
+  baseUrl: 'http://lymm-api.stateless-services.com'
+  // baseUrl: 'https://lymm-returns-api.herokuapp.com'
 }
